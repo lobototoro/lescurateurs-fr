977043e69903323d2668cc7273d570286668350b
pablo sinivassin <psinivassin@gmail.com>
2025-12-19 16:02:49 +0100

    exposing dev to external devices and adding suspense to homepage

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -12,16 +12,19 @@ function App() {
   const slugs = Route.useLoaderData();
 
   return (
-    <Suspense fallback={<h2>Loading...</h2>}>
-      {slugs.map((slug) => {
-        return (
-          <div key={slug.id}>
-            <Link to="/article/$slug" params={{ slug: slug.slug as string }}>
-              {slug.slug}
-            </Link>
-          </div>
-        );
-      })}
-    </Suspense>
+    <section className="flex flex-col p-6 lg:p-12">
+      <h1 className="text-4xl font-bold font-titles text-center w-full mb-3 lg:mb-9">Les Curateurs</h1>
+      <Suspense fallback={<h2>Loading...</h2>}>
+        {slugs.map((slug) => {
+          return (
+            <div key={slug.id}>
+              <Link to="/article/$slug" params={{ slug: slug.slug as string }}>
+                {slug.slug}
+              </Link>
+            </div>
+          );
+        })}
+      </Suspense>
+    </section>
   );
 }
