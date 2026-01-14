1ae617288571ec7caa2d2dff25cdd9f4d1015eb7
pablo sinivassin <psinivassin@gmail.com>
2026-01-14 08:14:42 +0100

    full function for navigation menu

--- src/routes/editor.tsx
+++ src/routes/editor.tsx
@@ -1,9 +1,11 @@
+import { useState } from "react";
 import HeaderMenu from "@/components/editor-components/headerMenu";
 import { Button } from "@/components/ui/button";
 import { createFileRoute, useNavigate } from "@tanstack/react-router";
 
 import { authClient } from "lib/auth/auth-client";
 import { authMiddleware } from "lib/auth/middleware";
+import { Separator } from "@/components/ui/separator";
 
 export const Route = createFileRoute("/editor")({
   component: RouteComponent,
@@ -13,6 +15,7 @@ export const Route = createFileRoute("/editor")({
 });
 
 function RouteComponent() {
+  const [selectedPermission, setSelectedPermission] = useState("");
   const { data: session } = authClient.useSession();
   const navigate = useNavigate();
   const logout = async () =>
@@ -31,7 +34,19 @@ function RouteComponent() {
     <div className="flex flex-col items-center justify-center w-full">
       {session && (
         <section className="w-full h-full m-auto">
-          <HeaderMenu role={(session.user as any)?.role || ""} permissions={(session.user as any)?.permissions} setSelection={() => {}} selection="" />
+          <h1 className="text-4xl font-bold font-titles text-center w-full mt-4 mb-4">Les Curateurs: editor</h1>
+          <div className="m-auto w-3/4">
+            <Separator className="my-4" />
+          </div>
+          <HeaderMenu
+            role={(session.user as any)?.role || ""}
+            permissions={(session.user as any)?.permissions}
+            setSelection={setSelectedPermission}
+            selection={selectedPermission}
+          />
+          <div className="m-auto w-3/4">
+            <Separator className="my-4" />
+          </div>
           <div className="place-self-stretch">
             <Button onClick={logout}>Logout</Button>
           </div>
